1fe925b0e29ed488cbf65add28b9ab38461df606
shivaram <shivaram.mamidi@bpmlinks.com>
2026-04-20 13:10:05 +0530

    feature branch and code change

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-(
-echo from flask import Flask
-echo.
-echo app = Flask(__name__)
-echo.
-echo @app.route("/")
-echo def home():
-echo     return "Hello from LinkedIn API ECS NonProd!"
-echo.
-echo if __name__ == "__main__":
-echo     app.run(host="0.0.0.0", port=5000)
-) > app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from LinkedIn API ECS NonProd!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
