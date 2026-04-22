9b2c25b9f1467b0a2ed5f25af78248a642a1a9ae
shivaram <shivaram.mamidi@bpmlinks.com>
2026-04-22 15:57:22 +0530

    sns notification changes

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import boto3
 
 app = Flask(__name__)
 
+sns = boto3.client("sns", region_name="us-east-1")
+
+TOPIC_ARN = "arn:aws:sns:us-east-1:935322646857:linkedin-nonprod-topic"
+
 @app.route("/")
 def home():
-    return "Hello from LinkedIn API ECS NonProd!"
+    # Send SNS email
+    sns.publish(
+        TopicArn=TOPIC_ARN,
+        Message="LinkedIn API was triggered!",
+        Subject="ECS Notification"
+    )
+
+    return "Hello Shivaram from LinkedIn API ECS NonProd!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
